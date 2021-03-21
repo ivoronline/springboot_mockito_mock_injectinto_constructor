0130f8d427d034100f070516a7d41fe3a0c4d283
igoreta <ivoronline@gmail.com>
2021-03-21 23:27:52 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_mockito_mock_injectinto_constructor/SpringbootMockitoMockInjectintoConstructorApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_mockito_mock_injectinto_constructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMockitoMockInjectintoConstructorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootMockitoMockInjectintoConstructorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_mockito_mock_injectinto_constructor/SpringbootMockitoMockInjectintoConstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_mockito_mock_injectinto_constructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMockitoMockInjectintoConstructorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
